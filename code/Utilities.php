90ff1e6bc1ca897e6e784f06f871c187df42fdf2
Leo Chen <leo@saltedherring.com>
2016-09-15 10:09:43 +1200

    Gravatar function introduced

--- code/Utilities.php
+++ code/Utilities.php
@@ -7,6 +7,35 @@
 namespace SaltedHerring;
 
 class Utilities {
+
+	/**
+	 * Get either a Gravatar URL or complete image tag for a specified email address.
+	 *
+	 * @param string $email The email address
+	 * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
+	 * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+	 * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+	 * @param boole $img True to return a complete IMG tag False for just the URL
+	 * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+	 * @return String containing either just a URL or a complete image tag
+	 * @source https://gravatar.com/site/implement/images/php/
+	 */
+	public static function Gravatar( $member_id, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+		if ($member = Member::get()->byID($member_id)) {
+		    $url = 'https://www.gravatar.com/avatar/';
+		    $url .= md5( strtolower( trim( $email ) ) );
+		    $url .= "?s=$s&d=$d&r=$r";
+		    if ( $img ) {
+		        $url = '<img src="' . $url . '"';
+		        foreach ( $atts as $key => $val )
+		            $url .= ' ' . $key . '="' . $val . '"';
+		        $url .= ' />';
+		    }
+		    return $url;
+		}
+
+		return null;
+	}
 	
 	public static function stripTags($strip_list, $html) {
 		foreach ($strip_list as $tag)
