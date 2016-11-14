21fca7cf72c30cc9c3b9e98d3cba6a52f7735ff8
Leo Chen <leo@saltedherring.com>
2016-11-13 16:04:52 -1200

    introducing salted cache

---
+++ code/SaltedCache.php
@@ -0,0 +1,29 @@
+<?php namespace SaltedHerring;
+
+class SaltedCache {
+    public static function read($factory, $cache_key) {
+		$cache			=	SS_Cache::factory($factory);
+		$cached			=	$cache->load($cache_key);
+
+		if (!empty($cached)) {
+			$cached		=	unserialize($cached);
+			return $cached;
+		}
+
+		return false;
+	}
+
+	public static function delete($factory, $cache_key) {
+		$cache			=	SS_Cache::factory($factory);
+		$cached			=	$cache->load($cache_key);
+		if (!empty($cached)) {
+			$cache->remove($cache_key);
+		}
+	}
+
+	public static function save($factory, $cache_key, $result) {
+		$cache			=	SS_Cache::factory($factory);
+		$cache_object	=	serialize($result);
+		$cache->save($cache_object, $cache_key);
+	}
+}
