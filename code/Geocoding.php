927b30a6050bcef6a4eeb4d13c868cf00aeea395
Leo Chen <leo@saltedherring.com>
2017-07-06 23:05:01 -1200

    added Guzzle; introduce Geocoding

---
+++ code/Geocoding.php
@@ -0,0 +1,54 @@
+<?php
+
+class Geocoding
+{
+    public static function LocToCoord($address, $api = '')
+    {
+        $client = new Client([
+            'base_uri' => 'https://maps.googleapis.com/maps/api/geocode/'
+        ]);
+
+        $query = array(
+            'address'   =>  $address
+        );
+
+        if (!empty($api)) {
+            $query['key'] = $api
+        }
+
+        $response = $client->request(
+            'GET',
+            'json',
+            array(
+                'query' => $query
+            )
+        );
+
+        return json_decode($response->getBody());
+    }
+
+    public static function CoordToLoc($lat, $lng, $api = '')
+    {
+        $client = new Client([
+            'base_uri' => 'https://maps.googleapis.com/maps/api/geocode/'
+        ]);
+
+        $query = array(
+            'latlng'   =>  $lat . ',' . $lng
+        );
+
+        if (!empty($api)) {
+            $query['key'] = $api
+        }
+
+        $response = $client->request(
+            'GET',
+            'json',
+            array(
+                'query' => $query
+            )
+        );
+
+        return json_decode($response->getBody());
+    }
+}
